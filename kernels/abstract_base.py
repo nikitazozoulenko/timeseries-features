changed from indices to torch.split views. Big speedup, faster than ksig now. Also, ksig is taking the mean of all truncation levels for the sig kernel, which causes a bug when normalizing

kernels/abstract_base.py
@@ -1,4 +1,6 @@
 from typing import List, Dict, Set, Any, Optional, Tuple, Literal, Callable
+import itertools
+
 import numpy as np
 import torch
 from joblib import Parallel, delayed
@@ -156,24 +158,27 @@ class TimeSeriesKernel():
         return False
 
 
-    def _batched_ker(
+    def _gram(
             self, 
             X: Tensor, 
-            Y: Tensor, 
+            Y: Tensor,
+            diag: bool,
         ):
         """
-        Method to be implemented by subclass. Computes the batched Gram matrix 
-        k(X_i, Y_i) for time series X_i and Y_i.
+        Method to be implemented by subclass. Computes the Gram matrix 
+        k(X_i, Y_j) for time series X_i and Y_j, or the diagonal k(X_i, Y_i)
+        if diag=True.
 
         Args:
-            X (Tensor): Tensor with shape (N, T, d).
-            Y (Tensor): Tensor with shape (N, T, d).
+            X (Tensor): Tensor with shape (N1, T, d).
+            Y (Tensor): Tensor with shape (N2, T, d).
+            diag (bool): If True, only computes the diagonal k(X_i, Y_i).
 
         Returns:
-            Tensor: Tensor with shape (N, ...) where (...) is the dimension 
-                of the kernel output.
+            Tensor: Tensor with shape (N1, N2, ...) or (N1, ...) if diag=True,
+             where  (...) is the dimension of the kernel output.
         """
-        raise NotImplementedError("Subclasses must implement '_batched_ker' method.")
+        raise NotImplementedError("Subclasses must implement '_gram' method.")
 
 
     def _max_batched_gram(
@@ -207,34 +212,30 @@ class TimeSeriesKernel():
         """
         N1, T, d = X.shape
         N2, _, _ = Y.shape
-        device = X.device
         max_batch = max_batch if max_batch is not None else self.max_batch
         normalize = normalize if normalize is not None else self.normalize
 
-        # get indices pairs
-        if diag:
-            indices = torch.arange(N1, device=device).tile(2,1) # shape (2, N)
-        else:
-            indices = torch.cartesian_prod(torch.arange(N1, device=device), 
-                                           torch.arange(N2, device=device)).T #shape (2, N1*N2)
-
         # split into batches
-        split = torch.split(indices, max_batch, dim=1)
+        split_X = torch.split(X, max_batch, dim=0)
+        Y_max_batch = max(1, max_batch//N1)
+        split_Y = torch.split(Y, Y_max_batch, dim=0)
+        split = zip(split_X, split_X) if diag else itertools.product(split_X, split_Y)
         result = Parallel(n_jobs=n_jobs)(
-            delayed(self._batched_ker)(X[ix], Y[iy])
-            for ix,iy in split)
-        result = torch.cat(result, dim=0)
-        extra = result[0].shape
+            delayed(self._gram)(x, y, diag) for x,y in split
+            )
 
         # reshape back
         if diag:
-            result = result.reshape( (N1,) + extra )
+            result = torch.cat(result, dim=0)
+        elif max_batch >= N1:
+            result = torch.cat(result, dim=1)
         else:
-            result = result.reshape( (N1, N2) + extra )
+            extra = result[0].shape[2:]
+            result = torch.cat(result, dim=0).reshape( (N1, N2)+extra )
     
         # normalize
         if normalize:
-            #Obtain the diagonals XX and YY
+            #Obtain the diagonals k(X,X) and K(Y,Y)
             if X is Y:
                 diagonal = torch.einsum('ii...->i...', result) #shape (N, ...)
                 XX = diagonal
@@ -246,7 +247,7 @@ class TimeSeriesKernel():
                 XX = XX[:, None] #shape (N1, 1, ...)
                 YY = YY[None, :] #shape (1, N2, ...)
 
-            # Normalize with diagonals
+            # Normalize with diagonals k(X,Y) = k(X,Y) / sqrt(k(X,X) * k(Y,Y))
             if self.log_space:
                 result = result - 0.5*XX - 0.5*YY
             else:

kernels/integral.py
@@ -13,6 +13,7 @@ from kernels.static_kernels import PolyKernel
 ################### Time series Integral Kernel of static kernel ######################
 #######################################################################################
 
+
 class StaticIntegralKernel(TimeSeriesKernel):
     def __init__(
             self,
@@ -31,14 +32,16 @@ class StaticIntegralKernel(TimeSeriesKernel):
         self.static_kernel = static_kernel
 
 
-    def _batched_ker(
+    #TODO implement for T1 != T2
+    def _gram(
             self, 
             X: Tensor, 
-            Y: Tensor, 
+            Y: Tensor,
+            diag: bool,
         ):
         # Shape (N, T)
-        iKt = self.static_kernel(X, Y, diag=True)
+        ijKt = self.static_kernel(X, Y, diag=True)
 
         #return integral of k(x_t, y_t) dt for each pair x and y
         T = X.shape[-2]
-        return torch.trapz(iKt, dx=1/(T-1), axis=-1)
+        return torch.trapz(ijKt, dx=1/(T-1), axis=-1)

kernels/sig_trunc.py
@@ -29,9 +29,8 @@ def cumsum_shift1(X:Tensor, dim:int):
     return Q
 
 
-# sligtly faster than the ksig library
 @torch.jit.script
-def trunc_sigker(
+def trunc_sigker_geoGEQ2(
         nabla:Tensor, 
         trunc_level:int, 
         geo_order:int,
@@ -47,11 +46,10 @@ def trunc_sigker(
         trunc_level (int): Truncation level of the signature.
         geo_order (int): Geometric order of the rough path lift.
     """
-    # A shape is (..., geo_order, geo_order, T1, T2)
     sh = nabla.shape
     A = torch.zeros(sh[:-2]+(geo_order, geo_order)+sh[-2:],
                     device=nabla.device, dtype=nabla.dtype)
-    results = torch.zeros( sh[:-2]+(trunc_level,),
+    results = torch.empty( sh[:-2]+(trunc_level,),
                           device=nabla.device, dtype=nabla.dtype)
     for n in range(trunc_level):
         AA = A.clone()
@@ -70,6 +68,36 @@ def trunc_sigker(
         # save
         results[..., n] = 1 + A.sum(dim = (-4, -3, -2, -1))
     
+    if only_last:
+        return results[..., -1]
+    else:
+        return results
+    
+
+@torch.jit.script
+def trunc_sigker_geo1(
+        nabla:Tensor, 
+        trunc_level:int, 
+        only_last:bool,
+    ):
+    """
+    Computes the non-geometric truncated signature kernel given a matrix 
+    nabla[s,t] = K[s+1, t+1] + K[s, t] - K[s+1, t] - K[s, t+1]. See Algo 
+    3 in https://jmlr.org/papers/v20/16-314.html.
+
+    Args:
+        nabla (Tensor): Matrix of shape (..., T1, T2).
+        trunc_level (int): Truncation level of the signature.
+    """
+    sh = nabla.shape
+    A = nabla.clone()
+    results = torch.empty(sh[:-2]+(trunc_level,),
+                          device=nabla.device, dtype=nabla.dtype)
+    results[..., 0] = 1 + A.sum(dim = (-2, -1))
+    for n in range(1, trunc_level):
+        A = nabla * cumsum_shift1(cumsum_shift1(A, dim=-2), dim=-1)
+        results[..., n] = 1 + A.sum(dim = (-2, -1))
+
     if only_last:
         return results[..., -1]
     else:
@@ -115,27 +143,16 @@ class TruncSigKernel(TimeSeriesKernel):
         self.only_last = only_last
 
 
-    def _batched_ker(
+    def _gram(
             self, 
             X: Tensor, 
-            Y: Tensor, 
+            Y: Tensor,
+            diag: bool,
         ):
-        """
-        Computes the Gram matrix K(X_i, Y_j), or the diagonal K(X_i, Y_i) 
-        if diag=True. The time series in X and Y are assumed to be of shape 
-        (T1, d) and (T2, d) respectively. O(T^2(d + trunc_level*geo_order^2)) 
-        time for each pair of time series.
-
-        Args:
-            X (Tensor): Tensor with shape (N, T1, d).
-            Y (Tensor): Tensor with shape (N, T2, d).
-            diag (bool, optional): If True, only computes the kernel for the 
-                pairs K(X_i, Y_i). Defaults to False.
-
-        Returns:
-            Tensor: Tensor with shape (N1, N2), or (N1) if diag=True.
-        """
         # nabla_st = K[s+1, t+1] + K[s, t] - K[s+1, t] - K[s, t+1] in time
-        K = self.static_kernel.time_gram(X, Y, diag=True)
+        K = self.static_kernel.time_gram(X, Y, diag)
         nabla = K.diff(dim=-1).diff(dim=-2) # shape (N, T1, T2)
-        return trunc_sigker(nabla, self.trunc_level, self.geo_order, self.only_last)
+        if self.geo_order >= 2:
+            return trunc_sigker_geoGEQ2(nabla, self.trunc_level, self.geo_order, self.only_last)
+        else:
+            return trunc_sigker_geo1(nabla, self.trunc_level, self.only_last)