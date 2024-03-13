fdf60822f9b880345a727e72d13a9e578abfbb13
Nikita Zozoulenko <nikita.zozoulenko@gmail.com>
2024-03-13 10:30:21 +0000

    added signatory integration

--- features/signature.py
+++ features/signature.py
@@ -13,10 +13,37 @@ def sig(
     shape (T,d) with optional batch support.
     
     Args:
-        X (Tensor): Tensor of shape (..., T, d) of time series.
+        X (Tensor): Tensor of shape (N, T, d) or (T, d) 
+            of time series.
         trunc_level (int): Signature truncation level.
     
     Returns:
-        Tensor: Tensor of shape (..., D) where 
+        Tensor: Tensor of shape (N, D) or (D) where 
             D = 1 + d + d^2 + ... + d^trunc_level.
     """
+    if len(X.shape) == 2:
+        X = X.unsqueeze(0)
+    return signatory.signature(X, trunc_level).squeeze(0)
+
+
+
+def logsig(
+    X: Tensor,
+    trunc_level: int,
+):
+    """
+    Computes the truncated log-signature of time series of
+    shape (T,d) with optional batch support.
+    
+    Args:
+        X (Tensor): Tensor of shape (N, T, d) or (T, d) 
+            of time series.
+        trunc_level (int): Signature truncation level.
+    
+    Returns:
+        Tensor: Tensor of shape (N, D) or (D) where D is 
+            O(d^trunc_level) but smaller than the signature.
+    """
+    if len(X.shape) == 2:
+        X = X.unsqueeze(0)
+    return signatory.logsignature(X, trunc_level).squeeze(0)
