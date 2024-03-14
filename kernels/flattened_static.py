b02af963958ba5714102fed960c3cf48cbab9983
Nikita Zozoulenko <nikita.zozoulenko@gmail.com>
2024-03-14 13:25:14 +0000

    implemented flattened time series kernel

---
+++ kernels/flattened_static.py
@@ -0,0 +1,44 @@
+from typing import List, Dict, Set, Any, Optional, Tuple, Literal, Callable
+import numpy as np
+import torch
+from torch import Tensor
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from kernels.abstract_base import StaticKernel, TimeSeriesKernel
+from kernels.static_kernels import RBFKernel
+
+#######################################################################################
+################### Time series Integral Kernel of static kernel ######################
+#######################################################################################
+
+
+class StaticIntegralKernel(TimeSeriesKernel):
+    def __init__(
+            self,
+            static_kernel:StaticKernel = RBFKernel(),
+            max_batch:int = 10000,
+            normalize:bool = False,
+        ):
+        """
+        Treats a time series as a big vector in R^(Td), where T is the 
+        length of the time series and d is the state-space dimension.
+
+        Args:
+            static_kernel (StaticKernel): Static kernel on R^d.
+        """
+        super().__init__(max_batch, normalize)
+        self.static_kernel = static_kernel
+
+
+    def _gram(
+            self, 
+            X: Tensor, 
+            Y: Tensor,
+            diag: bool,
+        ):
+        N = X.shape[0]
+        X_flat = X.reshape(N, -1)
+        Y_flat = Y.reshape(N, -1)
+        return self.static_kernel(X_flat, Y_flat, diag)
